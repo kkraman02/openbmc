tinhnampere/openbmc-dev

meta-ampere:mtmitchell: use nvmesensor for Front BP sensors
Replace NVME Temp sensor configuration by phosphor-nvme to use
dbus-sensor's nvmesensor

Tested:
1. Check nvme temperature sensors when plugs the NMVe to Front BP.

Signed-off-by: Thu Ba Nguyen <tbnguyen@amperecomputing.com>

meta-ampere/meta-mitchell/recipes-phosphor/configuration/dbus-sensors_%.bbappend
@@ -1,0 +1,4 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+PACKAGECONFIG:remove = " cpusensor ipmbsensor"
+PACKAGECONFIG:append = " nvmesensor"

kkraman02/openbmc

meta-ampere/meta-mitchell/recipes-phosphor/configuration/dbus-sensors_%.bbappend
@@ -1,0 +1,4 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+PACKAGECONFIG:remove = " intelcpusensor ipmbsensor"
+PACKAGECONFIG:append = " nvmesensor"